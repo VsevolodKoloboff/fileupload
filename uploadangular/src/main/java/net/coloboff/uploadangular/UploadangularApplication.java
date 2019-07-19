a31f743253f407f5ff4485ba63ca6f002eca738a
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2019-07-20 01:48:20 +0300

    init module

---
+++ uploadangular/src/main/java/net/coloboff/uploadangular/UploadangularApplication.java
@@ -0,0 +1,13 @@
+package net.coloboff.uploadangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadangularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadangularApplication.class, args);
+    }
+
+}
---
+++ uploadangular/src/test/java/net/coloboff/uploadangular/UploadangularApplicationTests.java
@@ -0,0 +1,16 @@
+package net.coloboff.uploadangular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UploadangularApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
